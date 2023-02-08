0abb35181b26747e437e4d478df43e6a75c44ba9
biotyree <afanasyev00@yandex.ru>
2023-02-08 17:52:01 +0300

    forceTask in Rocket comlete 1atempt

--- basic 2/Rocket/ForcesTask.cs
+++ basic 2/Rocket/ForcesTask.cs
@@ -4,24 +4,20 @@ using System;
 
 namespace func_rocket
 {
-	public class ForcesTask
-	{
-		/// <summary>
-		/// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
-		/// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
-		/// </summary>
-		public static RocketForce GetThrustForce(double forceValue)
-		{
-			return r => new Vector(Math.Cos(r.Direction) * forceValue, Math.Sin(r.Direction) * forceValue);
-		}
+    public class ForcesTask
+    {
+        /// <summary>
+        /// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
+        /// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
+        /// </summary>
+        public static RocketForce GetThrustForce(double forceValue) =>
+            r => new Vector(Math.Cos(r.Direction) * forceValue, Math.Sin(r.Direction) * forceValue);
 
-		/// <summary>
-		/// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
-		/// </summary>
-		public static RocketForce ConvertGravityToForce(Gravity gravity, Size spaceSize)
-		{
-			return r => gravity(spaceSize, r.Location);
-		}
+        /// <summary>
+        /// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
+        /// </summary>
+        public static RocketForce ConvertGravityToForce(Gravity gravity, Size spaceSize) =>
+            r => gravity(spaceSize, r.Location);
 
         /// <summary>
         /// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
